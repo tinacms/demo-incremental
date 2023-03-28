52d7057957a799dd1845049e8e5bbd4ffb896ff7
Scott Gallant <scottgallant@gmail.com>
2023-03-28 14:52:06 -0300

    removed unused code

--- .tina/config.tsx
+++ .tina/config.tsx
@@ -45,9 +45,6 @@ const config = defineConfig({
           //   if (document._sys.filename === "home") {
           //     return `/`;
           //   }
-          //   if (document._sys.filename === "about") {
-          //     return `/about`;
-          //   }
           //   return undefined;
           // },
         },
